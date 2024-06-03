c2fcda18c1d0920b6e4b9497e25096ce2e476eff
PseudoChord <sreerajsoff@gmail.com>
2024-06-03 18:54:16 +0530

    Add separate views for different users

--- book/models.py
+++ book/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Users(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     money = models.IntegerField(default=0)
--- book/views.py
+++ book/views.py
@@ -11,6 +11,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 
 
 
@@ -24,42 +27,46 @@ def index(request):
 
 
 
+@login_required
 def create_user(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            # Save the user data to the database
-            form.save()
-            return redirect('/view_users/')  # Redirect to a success page after saving
+            user_instance = form.save(commit=False)
+            user_instance.user = request.user  # Associate the user
+            user_instance.save()
+            return redirect('view_users')  # Redirect to a success page after saving
     else:
         form = UserForm()
     return render(request, 'create_user.html', {'form': form})
-
 
 def success(request):
     return render(request, 'success.html')
 
 
 def view_users(request):
-    users = Users.objects.all()  # Get all users
+    users = Users.objects.filter(user=request.user)  # Get only the logged-in user's data
     context = {'users': users}
     return render(request, 'view_users.html', context)
 
-class UserDeleteViews(DeleteView):
+class UserDeleteViews(LoginRequiredMixin, DeleteView):
     model = Users
     success_url = reverse_lazy('view_users')
 
+    def get_queryset(self):
+        # Ensure users can only delete their own data
+        return Users.objects.filter(user=self.request.user)
+
 def update_user(request, user_id):
-    user = Users.objects.get(pk=user_id)  # Get the user object
+    user_instance = Users.objects.get(pk=user_id, user=request.user)  # Get the user object
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)  # Pre-populate the form
+        form = UserForm(request.POST, instance=user_instance)  # Pre-populate the form
         if form.is_valid():
             form.save()
             return redirect('view_users')  # Redirect to the user list
     else:
-        form = UserForm(instance=user)  # Pre-populate the form with user data
+        form = UserForm(instance=user_instance)  # Pre-populate the form with user data
     return render(request, 'update_user.html', {'form': form})
-
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
@@ -67,9 +74,8 @@ from .loginform import LoginForm, RegistrationForm
 
 
 from django.shortcuts import render
-
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'user': request.user})
 
 def about(request):
     return render(request, 'about.html')
